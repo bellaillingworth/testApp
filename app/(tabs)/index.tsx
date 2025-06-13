94c612854670f946d6d8661e49b08b6f152307e7
Bella Illingworth <bpramjit@uiowa.edu>
2025-06-13 10:33:25 -0500

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -35,7 +35,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/icanLogo.png')}
+          source={require('@/assets/images/icanlogo.png')}
           style={styles.image}
           contentFit='contain'
         />
